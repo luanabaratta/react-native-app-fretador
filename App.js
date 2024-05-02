ccebc2407aa8d69fe8a40412b306740577f5f83d
Luana B. dos Santos <luanabaratta@Luanas-MacBook-Air.local>
2024-05-02 14:26:20 -0300

    feat: creating permission function and passing it to user effect hook

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from "react";
+import * as Location from 'expo-location';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    getLocationPermission();
+  }, []);
+  const getLocationPermission = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if(status !== 'granted') {
+      alert("Permissão negada!");
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync();
+    setLocation(location);
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
     </View>
   );
 }
